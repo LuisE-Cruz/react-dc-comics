b13a913d49fe7554cbc5210f5022c0acb51f0d0f
LuisE Cruz <Luis.cruzlc01@gmail.com>
2026-03-09 14:59:34 +0100

    Creata function RenderCard nel MainContentFirst, aggiunto badge e button a main first

--- src/Components/MainContentFirst.jsx
+++ src/Components/MainContentFirst.jsx
@@ -1,12 +1,32 @@
 import comics from "../assets/img/comics"
 
 function MainContentFirst() {
+
+    function RenderCard() {
+
+        return comics.map((comic) => (
+            <a key={comic.id} className="card" href="#">
+                <figure>
+                    <img src={comic.thumb} alt={comic.title} />
+                </figure>
+                <div className="card-title">
+                    {comic.series.toLocaleUpperCase()}
+                </div>
+            </a>
+        ))
+    }
+
     return (
         <section className="main-first">
             <div className="container-main-first">
-                <a className="link-main-first" href="#">
-                    {"--> Content goes here <--"}
-                </a>
+                <div className="badge">Current Series</div>
+                {RenderCard(comics)}
+
+            </div>
+
+            <div className="button-container">
+                <a className="button-main-first" href="#">Load More</a>
+
             </div>
         </section>
     )
