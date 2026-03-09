76691c9cc643552b3a9fa3f11eb0e561b40b8e4e
LuisE Cruz <Luis.cruzlc01@gmail.com>
2026-03-09 12:30:13 +0100

    Fine styling più impostazione di MainContentSecond, collegato tutto su MainContent e stampato su App.jsx

--- src/Components/MainContentSecond.jsx
+++ src/Components/MainContentSecond.jsx
@@ -1,6 +1,28 @@
 function MainContentSecond() {
     return (
-        <section>
+        <section className="main-second">
+            <ul className="list-main-second">
+                <li className="link-main-second">
+                    <img src="src/assets/img/buy-comics-digital-comics.png" alt="digital comics" />
+                    <a href="#">Digital Comics</a>
+                </li>
+                <li className="link-main-second">
+                    <img src="src/assets/img/buy-comics-merchandise.png" alt="Merchandise" />
+                    <a href="#">Dc Merchandise</a>
+                </li>
+                <li className="link-main-second">
+                    <img src="src/assets/img/buy-comics-subscriptions.png" alt="Subscription" />
+                    <a href="#">Subscription</a>
+                </li>
+                <li className="link-main-second">
+                    <img src="src/assets/img/buy-comics-shop-locator.png" alt="Shop Location" />
+                    <a href="#">Comic Shop Location</a>
+                </li>
+                <li className="link-main-second">
+                    <img src="src/assets/img/buy-dc-power-visa.svg" alt="Power visa" />
+                    <a href="#">Dc Power Visa</a>
+                </li>
+            </ul>
 
         </section>
     )
